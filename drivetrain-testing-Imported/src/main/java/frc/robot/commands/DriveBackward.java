eccf962c6f1019b47910f90bea0360999a1a0693
Calvin Macatantan <calmacatantan@gmail.com>
2022-10-21 16:33:30 -0400

    yay it works

---
+++ drivetrain-testing-Imported/src/main/java/frc/robot/commands/DriveBackward.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class DriveBackward extends CommandBase {
+  Drivetrain drivetrain;
+  
+  //@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  //private final ExampleSubsystem m_subsystem;
+
+  public DriveBackward(Drivetrain drivetrain) {
+    this.drivetrain = drivetrain;
+    // Use addRequirements() here to declare subsystem dependencies.
+    // addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drivetrain.drive(0,0);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drivetrain.drive(-0.2,0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetrain.drive(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
